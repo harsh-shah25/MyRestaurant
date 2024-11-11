4a7a8488f4b95306f161cf5814701a7257c2fb58
harsh-shah25 <2502harshshah@gmail.com>
2024-11-11 22:56:55 +0530

    3rd commit

--- src/main/java/com/harshshah/khaanakhazana/KhaanaKhazanaApplication.java
+++ src/main/java/com/harshshah/khaanakhazana/KhaanaKhazanaApplication.java
@@ -5,10 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
-public class YummyRestApplication {
+public class KhaanaKhazanaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(YummyRestApplication.class, args);
+		SpringApplication.run(KhaanaKhazanaApplication.class, args);
 	}
 
 }
--- src/test/java/com/harshshah/khaanakhazana/KhaanaKhazanaApplicationTests.java
+++ src/test/java/com/harshshah/khaanakhazana/KhaanaKhazanaApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class YummyRestApplicationTests {
+class KhaanaKhazanaApplicationTests {
 
 	@Test
 	void contextLoads() {
